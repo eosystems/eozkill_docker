157b8f451d3bd576e633bf9e69328e5fe1c852e1
r_nishio <quick.xp@me.com>
2017-05-14 18:51:42 +0900

    dev

--- build/mysql-eozkill/setting/init.sql
+++ build/mysql-eozkill/setting/init.sql
@@ -1,6 +1,6 @@
-create database eobackbone;
-create database eobackbone_development;
+create database eozkill;
+create database eozkill_development;
 
 CREATE USER eoservice;
-GRANT ALL ON eobackbone.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
-GRANT ALL ON eobackbone_development.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
+GRANT ALL ON eozkill.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
+GRANT ALL ON eozkill_development.* TO eoservice@'%' IDENTIFIED BY 'init' WITH GRANT OPTION;
